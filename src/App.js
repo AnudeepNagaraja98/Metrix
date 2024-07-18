09afd1326a96ec5842f028c21550802a4ba7348b
Anudeep Nagaraja <anudeepnagaraja@gmail.com>
2024-07-18 15:09:47 +0530

    CSS Resets and Font Family

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+// components
+import Child from "./Child";
+
+//stylesheet
 import "./App.css";
 
+//fonts
+import "./fonts/Poppins-Black.ttf";
+import "./fonts/Poppins-BlackItalic.ttf";
+import "./fonts/Poppins-Bold.ttf";
+import "./fonts/Poppins-BoldItalic.ttf";
+import "./fonts/Poppins-ExtraBold.ttf";
+import "./fonts/Poppins-ExtraBoldItalic.ttf";
+import "./fonts/Poppins-ExtraLight.ttf";
+import "./fonts/Poppins-ExtraLightItalic.ttf";
+import "./fonts/Poppins-Italic.ttf";
+import "./fonts/Poppins-Light.ttf";
+import "./fonts/Poppins-LightItalic.ttf";
+import "./fonts/Poppins-Medium.ttf";
+import "./fonts/Poppins-MediumItalic.ttf";
+import "./fonts/Poppins-Regular.ttf";
+import "./fonts/Poppins-SemiBold.ttf";
+import "./fonts/Poppins-SemiBoldItalic.ttf";
+import "./fonts/Poppins-Thin.ttf";
+import "./fonts/Poppins-ThinItalic.ttf";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Child />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Child.css";
+
+const Child = () => {
+  return <div className="Container">Child</div>;
+};
+
+export default Child;
