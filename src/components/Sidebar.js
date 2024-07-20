7c7da2e805c8abe1ec95ffa2549b93370d71ea87
Anudeep Nagaraja <anudeepnagaraja@gmail.com>
2024-07-20 18:20:40 +0530

    Sidebar Complete

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
-import React from "react";
-import "./Sidebar.css";
+import React, { useState } from "react";
+
+//! Metrix Icons
 import Metrix from "../icons/Metrix.svg";
 import Headphone from "../icons/Headphones.svg";
 import Gift from "../icons/Gift.svg";
@@ -11,8 +12,80 @@ import Category from "../icons/Category.svg";
 import Chat from "../icons/Chat.svg";
 import Folder from "../icons/Folder.svg";
 import Setting from "../icons/Setting.svg";
+import ActiveCategory from "../icons/Category-Active.svg";
+import ActiveBag from "../icons/Bag-Active.svg";
+import ActiveUser from "../icons/2-User-Active.svg";
+import ActiveFolder from "../icons/Folder-Active.svg";
+import ActiveChat from "../icons/Chat-Active.svg";
+import ActiveSetting from "../icons/Setting-Active.svg";
+
+//! CSS
+import "./Sidebar.css";
+
+const dashboardMenus = [
+  {
+    id: 0,
+    name: "Dashboard",
+    icon: Category,
+    active: ActiveCategory,
+    isActiveTab: true,
+    notification: null,
+  },
+  {
+    id: 1,
+    name: "Orders",
+    icon: Bag,
+    active: ActiveBag,
+    isActiveTab: false,
+    notification: 3,
+  },
+  {
+    id: 3,
+    name: "Customers",
+    icon: Users,
+    active: ActiveUser,
+    isActiveTab: false,
+    notification: null,
+  },
+  {
+    id: 4,
+    name: "Inventory",
+    icon: Folder,
+    active: ActiveFolder,
+    isActiveTab: false,
+    notification: null,
+  },
+  {
+    id: 5,
+    name: "Conversations",
+    icon: Chat,
+    active: ActiveChat,
+    isActiveTab: false,
+    notification: 16,
+  },
+  {
+    id: 6,
+    name: "Settings",
+    icon: Setting,
+    active: ActiveSetting,
+    isActiveTab: false,
+    notification: null,
+  },
+];
 
 const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState(dashboardMenus);
+  const handleClick = (val) => {
+    for (let i = 0; i < dashboardMenus.length; i++) {
+      if (val.id === dashboardMenus[i].id) {
+        dashboardMenus[i].isActiveTab = true;
+      } else {
+        dashboardMenus[i].isActiveTab = false;
+      }
+    }
+    setActiveTab([...dashboardMenus]);
+  };
+
   return (
     <div className="sidebar_container">
       <div className="logo_section">
@@ -21,38 +94,32 @@ const Sidebar = () => {
       </div>
       <div className="navigation_section">
         <div className="dashboard_wrapper">
-          <div className="dashboard_section active">
-            <img
-              src={Category}
-              alt="category_icon"
-              className="navigation_icons"
-            />
-            <p className="dashboard">Dashboard</p>
-          </div>
-          <div className="dashboard_section">
-            <img src={Bag} alt="Bag_icon" className="navigation_icons" />
-            <p className="dashboard">Orders</p>
-          </div>
-          <div className="dashboard_section">
-            <img src={Users} alt="Users_icon" className="navigation_icons" />
-            <p className="dashboard">Customers</p>
-          </div>
-          <div className="dashboard_section">
-            <img src={Folder} alt="Folder_icon" className="navigation_icons" />
-            <p className="dashboard">Inventory</p>
-          </div>
-          <div className="dashboard_section">
-            <img src={Chat} alt="Chat_icon" className="navigation_icons" />
-            <p className="dashboard">Chat</p>
-          </div>
-          <div className="dashboard_section">
-            <img
-              src={Setting}
-              alt="Setting_icon"
-              className="navigation_icons"
-            />
-            <p className="dashboard">Settings</p>
-          </div>
+          {activeTab &&
+            activeTab?.map((val) => {
+              return (
+                <div
+                  className={`dashboard_section ${
+                    val?.isActiveTab && "active"
+                  }`}
+                  key={val.id}
+                  onClick={() => handleClick(val)}
+                >
+                  <img
+                    src={val?.isActiveTab === true ? val?.active : val?.icon}
+                    alt="category_icon"
+                    className="navigation_icons"
+                  />
+                  <p className="dashboard">{val.name}</p>
+                  {val?.notification && (
+                    <div className="notification_wrapper">
+                      <p className="dashboard notification_number">
+                        {val?.notification}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
         </div>
       </div>
       <div className="accountupgrade_section">
