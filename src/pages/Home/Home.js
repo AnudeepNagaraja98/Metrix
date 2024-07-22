041b7db1d904bc07294b1c5226379a4aa56855f1
Anudeep Nagaraja <anudeepnagaraja@gmail.com>
2024-07-22 15:56:46 +0530

    OrdersAndSales Layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,7 +13,26 @@ const Home = () => {
         <div className="home_header">
           <Header />
         </div>
-        <div className="home_body">Body</div>
+        <div className="home_body">
+          <div className="sales_section">
+            <div className="sales_cards_section">
+              <div className="sales_card">Card</div>
+              <div className="sales_card">Card</div>
+            </div>
+            <div className="marketing_graph">
+              <div className="pie_graph">Pie Graph</div>
+              <div className="marketing_section">
+                <div className="marketing_card">Card</div>
+                <div className="marketing_card">Card</div>
+              </div>
+            </div>
+            <div className="summary_graph">Summary</div>
+          </div>
+          <div className="order_section">
+            <div className="order_card">Order Card</div>
+            <div className="recent_cards">Recent Card</div>
+          </div>
+        </div>
       </div>
     </div>
   );
